cf7da696248ee0aa7b7feb36ef1252b1626740f0
Nik Lutz <nik.lutz@gmail.com>
2010-11-23 00:08:11 +0000

    Versions update
    git-svn-id: https://veromix-plasmoid.googlecode.com/svn/trunk@48 3ea041cf-8e0d-0f7a-2218-2e5d71561eba

--- contents/code/main.py
+++ contents/code/main.py
@@ -59,7 +59,7 @@ from VeroMix import VeroMix
 from Utils import *
 
 class VeroMixPlasmoid(plasmascript.Applet):
-    VERSION="8.4"
+    VERSION="8.6"
 
     def __init__(self,parent,args=None):        
         plasmascript.Applet.__init__(self,parent)
