d8b7c05976a843c8310b917bf814f06b92c5e3e5
Nik Lutz <nik.lutz@gmail.com>
2010-11-26 00:31:14 +0000

    merged configuration dialogs - improved error-messages with link to the wiki
    git-svn-id: https://veromix-plasmoid.googlecode.com/svn/trunk@52 3ea041cf-8e0d-0f7a-2218-2e5d71561eba

--- contents/code/VeroMix.py
+++ contents/code/VeroMix.py
@@ -91,7 +91,8 @@ class VeroMix(QGraphicsWidget):
             self.showMessage(KIcon("script-error"), "There is a problem with the backgroud-service. \
                                                         <ul> \
                                                         <li>If you just upgraded try killing the process named: VeromixServiceMain.py and relaunch this plasmoid</li> \
-                                                        <li>If you don't know how to do that consider rebooting</li></ul>")
+                                                        <li>If you don't know how to do that consider rebooting</li></ul><br/>\
+                                                        <a href=\"http://code.google.com/p/veromix-plasmoid/wiki/VeromixComponents#The_service:_VeromixServiceMain.py\">See wiki for more details</a> <span style=\"font-size: small;\">(right click and copy url)</span>.")
             return
         self.connect(self.pa, SIGNAL("on_sink_input_info(PyQt_PyObject)"), self.on_sink_input_info)
         self.connect(self.pa, SIGNAL("on_sink_info(PyQt_PyObject)"), self.on_sink_info)
