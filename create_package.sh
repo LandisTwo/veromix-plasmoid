a34010aeda65b8ad39a0ceb89c43a6976cd075c5
Nik Lutz <nik.lutz@gmail.com>
2010-12-10 19:50:55 +0000

    debian package updated
    git-svn-id: https://veromix-plasmoid.googlecode.com/svn/trunk@118 3ea041cf-8e0d-0f7a-2218-2e5d71561eba

--- create_package.sh
+++ create_package.sh
@@ -2,13 +2,27 @@
 VERSION=$(cat metadata.desktop | grep X-KDE-PluginInfo-Version | awk 'BEGIN {FS= "="} ; { print $2 }')
 NAME=$(date +'%Y-%m-%d')
 NAME="${NAME}_${VERSION}_veromix.plasmoid"
-TAR_NAME="../plasma-widget-veromix_$VERSION.orig.tar.gz"
+TAR_NAME="plasma-widget-veromix_$VERSION.orig.tar.gz"
 
 find ./ -name '*~' | xargs rm
 find ./ -name '*.pyc' | xargs rm
 zip -r  ../$NAME * -x */*.svn/*
 plasmapkg -u ../$NAME
-rm $TAR_NAME
-tar zcvf $TAR_NAME --exclude=.svn  --exclude=debian --exclude="*.sh" --exclude="metadata.desktop.kde4.4" --exclude="*.mo" .
+rm ../$TAR_NAME
+tar zcvf ../$TAR_NAME --exclude=.svn  --exclude=debian --exclude="*.sh" --exclude="metadata.desktop.kde4.4" --exclude="*.mo" .
 echo $NAME
-
+ORIG=$(pwd)
+if [ "$1" = "debian" ] ; then
+    TMP=$(mktemp -d) 
+    cp ../$TAR_NAME  $TMP
+    BUILD="$TMP/plasma-widget-veromix/"
+    mkdir -p $BUILD
+    cp -R debian/ $BUILD
+    cp Messages.sh $BUILD/Messages.sh
+    cd $BUILD
+    tar -xzf $TMP/$TAR_NAME
+    dpkg-buildpackage -sa 
+    cd $ORIG
+    cp $TMP/* ../
+    echo $TMP
+fi
