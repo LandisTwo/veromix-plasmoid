d1b77d67414d1db8f4343d4399d3d416d329e751
Nik Lutz <nik.lutz@gmail.com>
2010-12-10 18:58:18 +0000

    git-svn-id: https://veromix-plasmoid.googlecode.com/svn/trunk@117 3ea041cf-8e0d-0f7a-2218-2e5d71561eba

--- create_package.sh
+++ create_package.sh
@@ -2,9 +2,13 @@
 VERSION=$(cat metadata.desktop | grep X-KDE-PluginInfo-Version | awk 'BEGIN {FS= "="} ; { print $2 }')
 NAME=$(date +'%Y-%m-%d')
 NAME="${NAME}_${VERSION}_veromix.plasmoid"
+TAR_NAME="../plasma-widget-veromix_$VERSION.orig.tar.gz"
+
 find ./ -name '*~' | xargs rm
 find ./ -name '*.pyc' | xargs rm
 zip -r  ../$NAME * -x */*.svn/*
 plasmapkg -u ../$NAME
+rm $TAR_NAME
+tar zcvf $TAR_NAME --exclude=.svn  --exclude=debian --exclude="*.sh" --exclude="metadata.desktop.kde4.4" --exclude="*.mo" .
 echo $NAME
 
