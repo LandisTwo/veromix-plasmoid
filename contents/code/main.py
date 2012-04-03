50e45564db7588b818bcda9001a533cb3185efbd
Nik Lutz <nik.lutz@gmail.com>
2012-04-04 00:20:10 +0200

    Updating version info.

--- contents/code/main.py
+++ contents/code/main.py
@@ -58,7 +58,7 @@ from Utils import *
 COMMENT=i18n("Veromix is a mixer for the Pulseaudio sound server. ")
 
 class VeroMixPlasmoid(plasmascript.Applet):
-    VERSION="0.16.1"
+    VERSION="0.16.2"
 
     nowplaying_player_added = pyqtSignal(QString, QObject)
     nowplaying_player_removed = pyqtSignal(QString)
