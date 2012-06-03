9a6739a43918f6d9e5b0d412535ed1c6cc637428
Nik Lutz <nik.lutz@gmail.com>
2012-06-04 00:58:53 +0200

    Mute button shows additional icon indicating mute-state.

--- gtk/Channel.py
+++ gtk/Channel.py
@@ -16,12 +16,14 @@
 
 import re
 from gi.repository import Gtk, Gdk
+from gi.repository.GdkPixbuf import Pixbuf
 
 from SliderWidget import SliderWidget
 from LadspaWidget import LadspaWidget
 from ContextMenu import ContextMenu
+from MuteButton import MuteButton
 from veromixcommon.MediaPlayer import MediaPlayer
-from gi.repository.GdkPixbuf import Pixbuf
+
 
 DRAG_ACTION = Gdk.DragAction.COPY
 (TARGET_ENTRY_TEXT, TARGET_ENTRY_PIXBUF) = range(2)
@@ -35,7 +37,6 @@ class Channel(Gtk.Alignment):
         #self.set_homogeneous(False)
         self._init()
         self.set_size_request(self.ICON_HEIGHT, self.ICON_HEIGHT)
-        self._current_icon = None
 
     def _init(self):
         self.frame = Gtk.Frame()
@@ -78,18 +79,8 @@ class Channel(Gtk.Alignment):
 
     def _create_mute(self):
         self.mute_box = Gtk.VBox()
-        self.mute = Gtk.ToggleButton()
-        self.mute.set_can_focus(False)
-        self.mute.set_relief(Gtk.ReliefStyle.NONE)
-        self.mute.set_image_position(1)
-        self.mute.connect("released", self.on_muted_clicked)
-        self.mute.set_size_request(self.ICON_HEIGHT,self.ICON_HEIGHT)
-
-    #def _create_mute(self):
-        ##self.mute = Gtk.ToggleButton()
-        #self.mute = Gtk.Image()
-        ##self.mute.set_image_position(1)
-        ##self.mute.connect("clicked", self.on_muted_clicked)
+        self.mute = MuteButton()
+        self.mute.connect_clicked(self.on_muted_clicked)
 
     def _create_slider(self):
         self.slider = SliderWidget()
@@ -119,12 +110,7 @@ class Channel(Gtk.Alignment):
 
         self.slider.set_volume(data)
         self.mute.set_active(data.isMuted())
-
-        if self._current_icon != data.get_nice_icon():
-            image = Gtk.Image()
-            image.set_from_icon_name(data.get_nice_icon(), Gtk.IconSize.BUTTON)
-            self.mute.set_image(image)
-            self._current_icon = data.get_nice_icon()
+        self.mute.set_image_name(data.get_nice_icon())
 
     def step_volume(self, up):
         self.slider.step_volume(up)
@@ -140,9 +126,6 @@ class SinkChannel(Channel):
     def _init(self):
         Channel._init(self)
         self.drag_dest_set(Gtk.DestDefaults.ALL, [], DRAG_ACTION)
-        #self.drag_dest_add_uri_targets()
-        #self.drag_dest_add_text_targets()
-        #self.drag_dest_add_uri_targets()
         self.drag_dest_add_text_targets()
         self.connect("drag-data-received", self.on_drag_data_received)
 
@@ -151,47 +134,28 @@ class SinkChannel(Channel):
             match =  re.match("veromix://sink_input_index:(\d*)", data.get_text())
             if match:
                 self.pa_sink_proxy().move_sink_input(match.group(1))
-                # print ("veromix text: %s" % match.group(1))
 
 class SinkInputChannel(Channel):
 
     def _init(self):
         Channel._init(self)
-        #self.set_padding(padding_top, padding_bottom, padding_left, padding_right)
         self.set_padding(0, 0, self.ICON_HEIGHT / 2, 0)
-
         self.drag_source_set(Gdk.ModifierType.BUTTON1_MASK, [], DRAG_ACTION)
-        self.mute.drag_source_set(Gdk.ModifierType.BUTTON1_MASK, [], DRAG_ACTION)
-        #self.label.drag_source_set(Gdk.ModifierType.BUTTON1_MASK, [], DRAG_ACTION)
-        #self.middle.drag_source_set(Gdk.ModifierType.BUTTON1_MASK, [], DRAG_ACTION)
-
         self.drag_source_add_text_targets()
-        #self.label.drag_source_add_uri_targets()
-        self.mute.drag_source_add_text_targets()
-
-        #self.drag_source_add_uri_targets()
-        #self.mute.drag_source_add_uri_targets()
-
         self.connect("drag-data-get", self.on_drag_data_get)
-        self.mute.connect("drag-data-get", self.on_drag_data_get)
-        #self.label.connect("drag-data-get", self.on_drag_data_get)
 
     def on_drag_data_get(self, widget, drag_context, data, info, time):
         data.set_text("veromix://sink_input_index:"+str(self.pa_sink_proxy().get_index()), -1)
-
 
 class SourceChannel(Channel):
 
     def _init(self):
         Channel._init(self)
-        #self.set_padding(padding_top, padding_bottom, padding_left, padding_right)
-#        self.set_padding(0, 0, self.ICON_HEIGHT / 2, 0)
 
 class SourceOutputChannel(Channel):
 
     def _init(self):
         Channel._init(self)
-        #self.set_padding(padding_top, padding_bottom, padding_left, padding_right)
         self.set_padding(0, 0, self.ICON_HEIGHT / 2, 0)
 
 class LadspaChannel(SinkChannel):
