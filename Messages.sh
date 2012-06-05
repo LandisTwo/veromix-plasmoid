8f979219f7ba391f38583ed6701a1f9f745c2707
Nik Lutz <nik.lutz@gmail.com>
2012-06-06 00:32:52 +0200

    Updating to new directory structure.

--- Messages.sh
+++ Messages.sh
@@ -7,16 +7,16 @@ XGETTEXT="xgettext -ki18n"
 EXTRACTRC="extractrc"
 
 if [ "x$1" != "x" ]; then
-    if [ ! -d "contents/locale/$1" ]; then
-        mkdir -p "contents/locale/$1/LC_MESSAGES"
+    if [ ! -d "plasma/contents/locale/$1" ]; then
+        mkdir -p "plasma/contents/locale/$1/LC_MESSAGES"
     fi
 fi
 
-$EXTRACTRC contents/ui/*.ui contents/config/*.xml > ./rc.py
-$XGETTEXT rc.py contents/code/*.py -o "$NAME.pot"
+$EXTRACTRC plasma/contents/ui/*.ui plasma/contents/config/*.xml > ./rc.py
+$XGETTEXT rc.py plasma/contents/code/*.py plasma/contents/code/veromixcommon/*.py -o "$NAME.pot"
 sed -e 's/charset=CHARSET/charset=UTF-8/g' -i "$NAME.pot"
 
-for d in contents/locale/*; do
+for d in plasma/contents/locale/*; do
     if [ -d "$d" ]; then
         if [ -f "$d/LC_MESSAGES/$NAME.po" ]; then
             echo "Merging $NAME.pot -> $d/LC_MESSAGES/$NAME.po ..."
@@ -28,7 +28,7 @@ for d in contents/locale/*; do
     fi
 done
 
-for d in contents/locale/*; do
+for d in plasma/contents/locale/*; do
     echo "Making $d/LC_MESSAGES/$NAME.mo ..."
     msgfmt "$d/LC_MESSAGES/$NAME.po" -o "$d/LC_MESSAGES/$NAME.mo"
 done
